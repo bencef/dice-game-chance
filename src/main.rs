85588d040e7e81f3ab4c8779c62e1cf90bc5b883
Bence Fábián <begnoc@gmail.com>
2019-07-08 22:39:55 +0200

    implement NdN+N

--- src/main.rs
+++ src/main.rs
@@ -81,9 +81,29 @@ pub mod dice {
         use std::iter::FromIterator;
         dist::uniform(Vec::from_iter(1..=sides))
     }
+
+    // TODO: use a macro to make `plus' optional
+    pub fn d(amount: u8, sides: u8, plus: u8) -> dist::Distribution<u8> {
+        let mut result = dist::always(0);
+        for _ in 0..amount {
+            result = summing(result,die(sides));
+        }
+        result.map(|orig| orig + plus)
+    }
+
+    fn summing(
+        orig_throws: dist::Distribution<u8>,
+        new_throws: dist::Distribution<u8>,
+    ) -> dist::Distribution<u8> {
+        fn add_two(orig: &u8) -> impl Fn(&u8) -> u8 {
+            let orig = orig.clone();
+            move |new| orig + new
+        }
+        new_throws.apply(orig_throws.map(add_two))
+    }
 }
 
 fn main() {
     use self::dice;
-    println!("Six sided die: {:?}", dice::die(6));
+    println!("2d4+3: {:?}", dice::d(2, 4, 3));
 }
